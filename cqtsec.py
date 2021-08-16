16b9307cf1f08865d5d3ceaaa130578e7a02edb6
Zafar Rafii <zafarrafii@gmail.com>
2021-08-16 11:55:28 -0700

    Added cqtdeconv example.

--- cqtsec.py
+++ cqtsec.py
@@ -4,7 +4,7 @@ This Python module implements the constant-Q transform spectral envelope coeffic
 Functions:
     mfcc - Compute the mel-frequency cepstral coefficients (MFCCs) (using librosa).
     cqtspectrogram - Compute the (magnitude) constant-Q transform (CQT) spectrogram (using librosa).
-    cqtdeconv - Compute the pitch-independent spectral envelope and the energy-normalized pitch component from the CQT spectrogram.
+    cqtdeconv - Deconvolve the constant-Q transform (CQT) spectrogram into a pitch-independent spectral envelope and an energy-normalized pitch component.
     cqtsec - Compute the CQT spectral envelope coefficients (CQT-SEC).
 
 Author:
@@ -34,6 +34,32 @@ def mfcc(
         number_coefficients: number of MFCCs (default: 20 coefficients)
     Output:
         audio_mfcc: audio MFCCs (number_coefficients, number_frames)
+
+    Example: Compute the MFCCs from an audio file.
+        # Import the modules
+        import numpy as np
+        import cqtsec
+        import librosa
+        import librosa.display
+        import matplotlib.pyplot as plt
+
+        # Load the audio signal
+        file_path = r'bass_acoustic_000-036-075.wav'
+        audio_signal, sampling_frequency = librosa.load(file_path, sr=None, mono=True)
+
+        # Define the parameters and compute the MFCCs
+        window_length = pow(2, int(np.ceil(np.log2(0.04 * sampling_frequency))))
+        step_length = int(window_length / 2)
+        number_coefficients = 20
+        audio_mfcc = cqtsec.mfcc(audio_signal, sampling_frequency, window_length, step_length, number_coefficients)
+
+        # Display the MFCCs
+        plt.figure(figsize=(14, 4))
+        librosa.display.specshow(audio_mfcc, x_axis='time', sr=sampling_frequency, hop_length=step_length, cmap='jet')
+        plt.title('MFCCs')
+        plt.ylabel('Coefficient')
+        plt.tight_layout()
+        plt.show()
     """
 
     # Compute the MFCCs using librosa's mfcc
@@ -66,6 +92,34 @@ def cqtspectrogram(
         octave_resolution: number of frequency channels per octave (default: 12 frequency channels per octave)
     Output:
         cqt_spectrogram: magnitude CQT spectrogram (number_frequencies, number_frames)
+
+    Example: Compute the CQT spectrogram from an audio file.
+        # Import the modules
+        import numpy as np
+        import cqtsec
+        import librosa
+        import librosa.display
+        import matplotlib.pyplot as plt
+
+        # Load the audio signal
+        file_path = r'bass_acoustic_000-036-075.wav'
+        audio_signal, sampling_frequency = librosa.load(file_path, sr=None, mono=True)
+
+        # Define the parameters and compute the CQT spectrogram
+        step_length = int(pow(2, int(np.ceil(np.log2(0.04 * sampling_frequency)))) / 2)
+        minimum_frequency = 32.70
+        octave_resolution = 12
+        cqt_spectrogram = cqtsec.cqtspectrogram(audio_signal, sampling_frequency, step_length, minimum_frequency, \
+                                                octave_resolution)
+
+        # Display the CQT spectrogram
+        plt.figure(figsize=(14, 4))
+        librosa.display.specshow(librosa.amplitude_to_db(cqt_spectrogram), x_axis='time', y_axis='cqt_note', \
+                                sr=sampling_frequency, hop_length=step_length, fmin=minimum_frequency, \
+                                bins_per_octave=octave_resolution, cmap='jet')
+        plt.title('CQT spectrogram')
+        plt.tight_layout()
+        plt.show()
     """
 
     # Derive the number of frequency channels
@@ -91,13 +145,53 @@ def cqtspectrogram(
 
 def cqtdeconv(cqt_spectrogram):
     """
-    Compute the pitch-independent spectral envelope and the energy-normalized pitch component from the constant-Q transform (CQT) spectrogram.
+    Deconvolve the constant-Q transform (CQT) spectrogram into a pitch-independent spectral envelope and an energy-normalized pitch component.
 
     Inputs:
         cqt_spectrogram: CQT spectrogram (number_frequencies, number_frames)
     Output:
         cqt_envelope: pitch-independent spectral envelope (number_frequencies, number_frames)
         cqt_pitch: energy-normalized pitch component (number_frequencies, number_frames)
+
+    Example: Deconvolve a CQT spectrogram into its spectral envelope and pitch component.
+        # Import the modules
+        import numpy as np
+        import cqtsec
+        import librosa
+        import librosa.display
+        import matplotlib.pyplot as plt
+
+        # Load the audio signal
+        file_path = r'bass_acoustic_000-036-075.wav'
+        audio_signal, sampling_frequency = librosa.load(file_path, sr=None, mono=True)
+
+        # Define the parameters and compute the CQT spectrogram
+        step_length = int(pow(2, int(np.ceil(np.log2(0.04 * sampling_frequency)))) / 2)
+        minimum_frequency = 32.70
+        octave_resolution = 12
+        cqt_spectrogram = cqtsec.cqtspectrogram(audio_signal, sampling_frequency, step_length, minimum_frequency, \
+                                                octave_resolution)
+
+        # Deconvolve the CQT spectrogram into a CQT envelope and pitch
+        cqt_envelope, cqt_pitch = cqtsec.cqtdeconv(cqt_spectrogram)
+
+        # Display the CQT spectrogram, envelope, and pitch
+        plt.figure(figsize=(14, 4))
+        plt.subplot(1, 3, 1)
+        librosa.display.specshow(librosa.amplitude_to_db(cqt_spectrogram), x_axis='time', y_axis='cqt_note', \
+                                sr=sampling_frequency, hop_length=step_length, fmin=minimum_frequency, \
+                                bins_per_octave=octave_resolution, cmap='jet')
+        plt.title('CQT spectrogram')
+        plt.subplot(1, 3, 2)
+        librosa.display.specshow(librosa.amplitude_to_db(cqt_envelope), x_axis='time', y_axis='cqt_note', sr=sampling_frequency, 
+                                hop_length=step_length, fmin=minimum_frequency, bins_per_octave=octave_resolution, cmap='jet')
+        plt.title('CQT envelope')
+        plt.subplot(1, 3, 3)
+        librosa.display.specshow(cqt_pitch, x_axis='time', y_axis='cqt_note', sr=sampling_frequency, hop_length=step_length, \
+                                fmin=minimum_frequency, bins_per_octave=octave_resolution, cmap='jet')
+        plt.title('CQT pitch')
+        plt.tight_layout()
+        plt.show()
     """
 
     # Get the number of frequency channels
